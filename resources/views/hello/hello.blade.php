53f71905c4cf3af2843ebba1d87279d8b0854ad9
youkuiyuan <youkuiyuan@leyou.com>
2017-03-03 10:16:50 +0800

    更新

---
+++ resources/views/hello/hello.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+    <head>
+        <meta charset="utf-8">
+        <title>基本结构</title>
+    </head>
+    <body>
+        <h1>Hello Laravel</h1>
+        <a href="http://www.baidu.com">百度</a>
+    </body>
+</html>
