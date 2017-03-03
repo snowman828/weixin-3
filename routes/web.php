a4adf3f84aef50f5dbf98085f13adb13e1a0c1d2
youkuiyuan <youkuiyuan@outlook.com>
2017-03-03 23:24:42 +0800

    更新

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,9 @@ FORM;
 
 
 Route::post('hello',function(){
-    $api = resolve('HelpSpot\API');
+    //Class HelpSpot\API does not exist
+    //Class 'App\Providers\HelpSpot\API' not found
+    $api = resolve('ViewServiceProvider');
     var_dump($api);
     return view('hello.hello', ['testStr' => strval('2017-03-03 23:50:50')]);
 });
