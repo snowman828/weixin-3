acc60f0b77dd3f47adc22bf032749fbbfeb9183a
youkuiyuan <youkuiyuan@leyou.com>
2017-03-17 11:29:15 +0800

    更新

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\Controller;
 class AdminController extends Controller
 {
     public function index(){
-        return redirect()->route('admin.show');
+        return route('admin.show');
     }
 
     public function show(){
