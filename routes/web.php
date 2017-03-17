027ae8aa43606df0d0def783289594964616a172
youkuiyuan <youkuiyuan@leyou.com>
2017-03-17 16:47:36 +0800

    更新

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 
 //后台
-Route::group(['namespace'=>'Admin'],function(){
+Route::group(function(){
     Route::controllers([
         'index'=>'AdminController@index',
         'login'=>'AdminController@login',
